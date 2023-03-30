513e58e62a73eec856c6ee556ee9d2aaf21d3410
Quochuynh <letrinhquoch@gmail.com>
2023-03-30 18:11:39 +0700

    next commit

--- src/main/java/com/s3925997/asm1/util/AppUtility.java
+++ src/main/java/com/s3925997/asm1/util/AppUtility.java
@@ -1,76 +1,82 @@
-package com.s3925997.asm1.util;
+// package com.s3925997.asm1.util;
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-public class AppUtility {
-    public AppUtility() {
-    }
+// public class AppUtility {
+// public AppUtility() {
+// }
 
-    public String getStringInput() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print(">>> ");
-        String inputString = scanner.nextLine();
-        return inputString;
-    }
+// public String getStringInput() {
+// Scanner scanner = new Scanner(System.in);
+// System.out.print(">>> ");
+// String inputString = scanner.nextLine();
+// return inputString;
+// }
 
-    public int getIntInput() {
-        boolean done = false;
-        int inputInt = 0;
-        while (!done) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.print(">>> ");
-                inputInt = scanner.nextInt();
-                if (inputInt < 0) {
-                    System.out.println("Invalid value entered. Must be a non-negative integer. Please try again.");
-                } else {
-                    done = true;
-                }
-            } catch (Exception e) {
-                System.out.println("Invalid value entered. Must be a non-negative integer. Please try again.");
-            }
-        }
-        return inputInt;
-    }
+// public int getIntInput() {
+// boolean done = false;
+// int inputInt = 0;
+// while (!done) {
+// try {
+// Scanner scanner = new Scanner(System.in);
+// System.out.print(">>> ");
+// inputInt = scanner.nextInt();
+// if (inputInt < 0) {
+// System.out.println("Invalid value entered. Must be a non-negative integer.
+// Please try again.");
+// } else {
+// done = true;
+// }
+// } catch (Exception e) {
+// System.out.println("Invalid value entered. Must be a non-negative integer.
+// Please try again.");
+// }
+// }
+// return inputInt;
+// }
 
-    public int getIntInput(int i1, int i2) {
-        boolean done = false;
-        int inputInt = 0;
-        while (!done) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.print(">>> ");
-                inputInt = scanner.nextInt();
-                if ((inputInt < 0) || !(i1 <= inputInt && inputInt < i2)) {
-                    System.out.println("Invalid value entered. Must be a non-negative integer in range ("
-                            + String.valueOf(i1) + "," + String.valueOf(i2) + "). Please try again.");
-                } else {
-                    done = true;
-                }
-            } catch (Exception e) {
-                System.out.println("Invalid value entered. Must be a non-negative integer. Please try again.");
-            }
-        }
-        return inputInt;
-    }
+// public int getIntInput(int i1, int i2) {
+// boolean done = false;
+// int inputInt = 0;
+// while (!done) {
+// try {
+// Scanner scanner = new Scanner(System.in);
+// System.out.print(">>> ");
+// inputInt = scanner.nextInt();
+// if ((inputInt < 0) || !(i1 <= inputInt && inputInt < i2)) {
+// System.out.println("Invalid value entered. Must be a non-negative integer in
+// range ("
+// + String.valueOf(i1) + "," + String.valueOf(i2) + "). Please try again.");
+// } else {
+// done = true;
+// }
+// } catch (Exception e) {
+// System.out.println("Invalid value entered. Must be a non-negative integer.
+// Please try again.");
+// }
+// }
+// return inputInt;
+// }
 
-    public double getDoubleInput() {
-        boolean done = false;
-        double inputDouble = 0;
-        while (!done) {
-            try {
-                Scanner scanner = new Scanner(System.in);
-                System.out.print(">>> ");
-                inputDouble = scanner.nextDouble();
-                if (inputDouble < 0) {
-                    System.out.println("Invalid value entered. Must be non-negative. Please try again.");
-                } else {
-                    done = true;
-                }
-            } catch (Exception e) {
-                System.out.println("Invalid value entered. Must be non-negative. Please try again.");
-            }
-        }
-        return inputDouble;
-    }
-}
+// public double getDoubleInput() {
+// boolean done = false;
+// double inputDouble = 0;
+// while (!done) {
+// try {
+// Scanner scanner = new Scanner(System.in);
+// System.out.print(">>> ");
+// inputDouble = scanner.nextDouble();
+// if (inputDouble < 0) {
+// System.out.println("Invalid value entered. Must be non-negative. Please try
+// again.");
+// } else {
+// done = true;
+// }
+// } catch (Exception e) {
+// System.out.println("Invalid value entered. Must be non-negative. Please try
+// again.");
+// }
+// }
+// return inputDouble;
+// }
+// }
